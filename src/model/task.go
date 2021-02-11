8e36820c4fa49da6f1532611c8c3436a88d41514
k18083kk <k18083kk@pck18083kk.local>
2021-02-12 01:22:55 +0900

    change

--- src/model/task.go
+++ src/model/task.go
@@ -6,7 +6,7 @@ import (
 
 type Task struct {
 	ID       uint      `json:"id"`
-	CreateAt time.Time `json:"created_at"`
+	CreateAt time.Time `json:"create_at"`
 	UpdateAt time.Time `json:"updated_at"`
 	Title    string    `json:"title"`
 }
